472da281ef73138ce3dda55754a23d084e6053ed
JJongmen <pjy90123@gmail.com>
2019-11-21 16:43:26 +0900

    broken letter fix please

--- postfix.c
+++ postfix.c
@@ -1,34 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "calc.h" 
 
 char *postfix(char *infix)
 {
 	char *str;
-	str = malloc(sizeof(char)*strlen(infix));
-	stack <char> operator;
-	for(infix; infix != '\0'; infix++) {
-		if (97 <= *infix <= 122) strncat(str, infix, 1);
+	str = (char *) malloc(sizeof(char)*strlen(infix)+1);
+	memset(str, 0, sizeof(char)*strlen(infix)+1);
+	struct node* head = NULL; //stack <char> operator;
+	char temp;
+	int count = 1;
+
+	//push(&top, 'C');
+	while (*infix != '\0') {
+			printf("infix[%d]:%c\n",count,*infix);
+		if (97 <= *infix && *infix <= 122) {
+			strncat(str, infix, 1);
+			printf("strncat:%s\n",str);
+		}
 		else {
 			if (*infix == '+' || *infix == '-') {
-				if (operator.top() == '+' || operator.top() == '-') operator.push(*infix);
-				else if (operator.top()  == '*' || operator.top() == '/') {
-					temp = operator.top();
-					operator.pop();
-					str += temp;
-					operator.push(*infix);
+				if (top(head) == '+' || top(head) == '-' || empty(head) || top(head) == '(') push(&head, *infix);
+				else if (top(head) == '*' || top(head) == '/') {
+					temp = top(head);
+					pop(&head);
+					strcat(str,&temp);
+					push(&head, *infix);
 				}
-			else if (*infix == '*' || *infix == '/') operator.push(*infix);
-			else if (*infix == '(') operator.push(*infix);
-			else if (*infix == ')') {
-				while (operator.top() != '(') {
-					temp = operator.top();
-					operator.pop();
-					str += temp;
-				}
-				operator.pop();
 			}
+			else if (*infix == '*' || *infix == '/') push(&head, *infix);
+			else if (*infix == '(') push(&head, *infix);
+			else if (*infix == ')') {
+				while (top(head) != '(') {
+					temp = top(head);
+					pop(&head);
+					strcat(str,&temp);
+				}
+				pop(&head);
+			}
+		}
+		count++;
+		infix++;
+		display(head);
 	}
-	free(str);
+	while (!empty(head)) {
+		temp = top(head);
+		pop(&head);
+		strcat(str,&temp);
+		printf("%s\n", str);
+	}
+	//free(str);
 	return str;
+}
+
+int main(void) {
+	char* result;
+	printf("%s\n", result = postfix("a+b*c"));
+	free(result);
+	return 0;
 }
