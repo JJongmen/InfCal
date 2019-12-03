baaf61243f642b848a90d0a876ba5778abc8c2b3
JJongmen <pjy90123@gmail.com>
2019-12-04 00:29:26 +0900

    garbage X

--- postfix.c
+++ postfix.c
@@ -7,10 +7,13 @@ char *postfix(char *infix)
 {
 	char *str;
 	str = (char *) malloc(2*sizeof(char)*strlen(infix)+1);
-	memset(str, 0, sizeof(char)*strlen(infix)+1);
+	memset(str, 0, 2*sizeof(char)*strlen(infix)+1);
+	printf("str:%s\n", str);
 	struct node* head = NULL; //stack <char> operator;
-	char temp;
-	char space = ' ';
+	char* temp;
+	temp = (char *) malloc(sizeof(char));
+	//char space = ' ';
+	char* space = " ";
 	int count = 1;
 
 	//push(&top, 'C');
@@ -18,17 +21,16 @@ char *postfix(char *infix)
 			printf("infix[%d]:%c\n",count,*infix);
 		if (97 <= *infix && *infix <= 122) {
 			strncat(str, infix, 1);
-			strcat(str,&space);
-			printf("strncat:%s\n",str);
+			strncat(str, space, 1);
 		}
 		else {
 			if (*infix == '+' || *infix == '-') {
 				if (top(head) == '+' || top(head) == '-' || empty(head) || top(head) == '(') push(&head, *infix);
 				else if (top(head) == '*' || top(head) == '/') {
-					temp = top(head);
+					*temp = top(head);
 					pop(&head);
-					strcat(str,&temp);
-					strcat(str,&space);
+					strncat(str,temp,1);
+					strncat(str,space,1);
 					push(&head, *infix);
 				}
 			}
@@ -36,10 +38,10 @@ char *postfix(char *infix)
 			else if (*infix == '(') push(&head, *infix);
 			else if (*infix == ')') {
 				while (top(head) != '(') {
-					temp = top(head);
+					*temp = top(head);
 					pop(&head);
-					strcat(str,&temp);
-					strcat(str,&temp);
+					strncat(str,temp,1);
+					strncat(str,space,1);
 
 				}
 				pop(&head);
@@ -50,10 +52,10 @@ char *postfix(char *infix)
 		display(head);
 	}
 	while (!empty(head)) {
-		temp = top(head);
+		*temp = top(head);
 		pop(&head);
-		strcat(str,&temp);
-		strcat(str,&space);
+		strncat(str,temp,1);
+		strncat(str,space,1);
 		printf("%s\n", str);
 	}
 	//free(str);
@@ -62,7 +64,7 @@ char *postfix(char *infix)
 
 int main(void) {
 	char* result;
-	printf("%s\n", result = postfix("a+b*c"));
+	printf("%s\n", result = postfix("a+b*(c-d)"));
 	free(result);
 	return 0;
 }
