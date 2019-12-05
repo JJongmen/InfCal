ea4caca7b72ffdc30b0f613695cc7436ddd0ce15
JJongmen <pjy90123@gmail.com>
2019-12-05 17:55:26 +0900

    getc example

--- test.c
+++ test.c
@@ -1,53 +1,12 @@
-#include <stdio.h>
-#include <string.h>
+#include "calc.h"
 
 int main(void){
-
-        char str[] = "11 222 33 444 + - * ";
-		
-
-        char *ptr;
-		char *one;
-		char *two;
-        int tmp;
-
-
-
-        printf("함수 호출 전의 스트링 : %s\n" , str) ;
-
-
-
-        //ptr = strtok(str, ",");
-
-        //printf("%s\n" , ptr);
-
-
-
-        ptr = strtok(str, " ");
-		two = ptr;
-
-
-
-        while(ptr != NULL ){
-
-
-
-                printf( "%s\n" , ptr);
-				one = two;
-				two = ptr;
-                ptr = strtok(NULL, " ");
-				
-				if (*ptr == '+'||*ptr == '-'||*ptr =='*') printf("one: %s ,two: %s, ptr = %s\n",one,two,ptr);
-
-				
-        }
-		// 11 222 33 444 + - *
-		// 11 222 (33 444 +) - *
-        //printf("함수 호출 후의 스트링 : %s\n" , str);
-        
-        return 0;
-
+	int ch;
+	ch = getc(stdin);
+	while(1){
+		if (ch == EOF) break;
+		printf("ch: %c\n", ch);
+		ch = getc(stdin);
+	}
+	return 0;
 }
-
-
-
