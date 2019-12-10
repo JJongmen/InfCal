1760ac03d2f1093caf088554a620a867bc2921eb
JJongmen <pjy90123@gmail.com>
2019-12-10 18:31:31 +0900

    a

---
+++ calc.c
@@ -0,0 +1,23 @@
+#include "calc.h"
+
+LINK add(LINK a, LINK b);
+LINK minus(LINK a, LINK b);
+LINK multiply(LINK a, LINK b);
+LINK calc(LINK str);
+
+LINK calc(LINK str)
+{
+	//재원이형 seccalc.c 
+}
+
+int main(void) {
+	printf("Enter the expression in infix notation.\n
+			Input : ");
+	//print_list 
+	while (!check(str)) {
+		
+	}
+	str = postfix(str);
+	answer = calc(str);
+	pritnf("%s\n",answer);
+}
