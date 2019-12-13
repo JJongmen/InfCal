808489dc8819aef229266c0df560a32eb6641365
GoodTY <taek3153@gmail.com>
2019-12-14 01:24:55 +0900

    3칙연산 더하기 진행중

--- 3_fund_cal.c
+++ 3_fund_cal.c
@@ -23,12 +23,46 @@ two = (b-> head -> data != '-'?1:0);
 
 if ( one == 1 && one == 1);
 
-// 계산
+if (a_count > b_count){
+   count = a_count;
+   a_point =  
+   b_point = 
+   a_int_len = a_point - 1;
+   b_int_len = b_point - 1;
+   a_dec_len = b_count - a_point;
+   b_dec_len = b_count - b_point;
+   while ( a_int_len = b_int_len) // int 자리수 맞추기
+   {
+	   if (a_int_len > b_int_len) {
+		   b head 에 0 추가;
+		   b_int_len++;
+	   }
+	   else if (a_int_len < b_int_len) {
+		   a head 에 0추가;
+		   a_int_len++;
+	   }
+	}
+   while (a_dec_len = b_dec_len) // dec 자리수 맞추기
+   {
+	   if (a_dec_len > b_dec_len) {
+		   b tail 에 0 추가;
+		   b_dec_len++;
+	   }
+	   else if (a_dec_len < b_dec_len) {
+		   a tail 에 0 추가;
+		   a_dec_len++;
+	   }
+   }
+ // 999.9990 + 009.9999
+   LINK c = 연결리스트 생성 count +1 개수  만큼 0으로 채워야 한다.
+   for 문으로 a_head + b_head 덧셈 
+   if a_head + b_head >= 10
+   
+   }
 
 else if ( one == 1 && two == 0) { // add( a,-b )
 
 	deleteData(Link b, char '-')
-
 	return minus(a,b);
 	}
 
@@ -74,7 +108,7 @@ else if ( one == 0 && two == 1) { // minus(-a,b)
 	return insertFront(List add( +a ,+b ), # 노드 '-')
 	}
 
-else if ( one == 0 && two == 0) { // minus( -a , +b )
+else if ( one == 0 && 가two == 0) { // minus( -a , +b )
 
 	deleteData(Link b, char'-')
 
