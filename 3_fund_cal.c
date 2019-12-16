e843b0fe7f00f6732daa6e62a1a3a02025022126
GoodTY <taek3153@gmail.com>
2019-12-16 18:16:23 +0900

    더하기 뺄셈 완성

--- 3_fund_cal.c
+++ 3_fund_cal.c
@@ -26,22 +26,15 @@ pList add(pList a , pList b ) {
 	b_head = (b -> head -> data)
 
 	if ( one == 1 && one == 1){
-		if (searchNode(pList a, '.') == 0){
-			insertBack(pList a, makeNode('.'));
+		if (searchNode(a, '.') == 0){
+			insertBack(a, makeNode('.'));
 		}
-		if (searchNode(pList b, '.') == 0){
-			insertBack(pList b, makeNode('.'));
+		if (searchNode(b, '.') == 0){
+			insertBack(b, makeNode('.'));
 		}
 	// 자연수일 경우 . 추가
-
-		if (a_count > b_count){
-   			count = a_count;
-		}
-		if (a_count < b_count){
-			count = b_count;
-		}
-   		a_point = searchNode(pList a, '.') 
-   		b_point = searchNode(pList a, '.')
+   		a_point = searchNode(a, '.') 
+   		b_point = searchNode(a, '.')
    		a_int_len = a_point - 1;
    		b_int_len = b_point - 1;
    		a_dec_len = a_count - a_point;
@@ -49,22 +42,22 @@ pList add(pList a , pList b ) {
    		while ( a_int_len != b_int_len) // int 자리수 맞추기
 		{
 	   		if (a_int_len > b_int_len) {
-		   		insertFront(pList b, makeNode('0'));
+		   		insertFront(b, makeNode('0'));
 		   		b_int_len++;
 			}
 	   		else if (a_int_len < b_int_len) {
-		   		insertFront(pList a, makeNode('0'));
+		   		insertFront(a, makeNode('0'));
 		   		a_int_len++;
 			}
 		}
    		while (a_dec_len != b_dec_len) // dec 자리수 맞추기
 		{
 	   		if (a_dec_len > b_dec_len) {
-		   		insertBack(pList b, makeNode('0'));
+		   		insertBack(b, makeNode('0'));
 		   		b_dec_len++;
 			}
 	   		else if (a_dec_len < b_dec_len) {
-		   		insertBack(pList a, makeNode('0'));
+		   		insertBack(a, makeNode('0'));
 		   		a_dec_len++;
 			}
 		}
@@ -73,48 +66,48 @@ pList add(pList a , pList b ) {
 		pList answer = makeList();
 		while (a_head != '\0'){
 			if (a_head - '0' + b_head - '0'  > 9) {
-				insertBack(pList pst, makeNode('1'));
-				insertBack(pList answer, makeNode(a_head + b_head - 2 * '0'));
-				deleteHead(pList a);
-				deleteHead(pList b);
+				insertBack(pst, makeNode('1'));
+				insertBack(answer, makeNode(a_head + b_head - 2 * '0'));
+				deleteHead(a);
+				deleteHead(b);
 			}
 			else if (a_head - '0' + b_head - '0'  < 10) {
-				insertBack(pList pst , makeNode('0'));
-				insertBack(pList answer, makeNode(a_head + b_head));
-				deleteHead(pList a);
-				deleteHead(pList b);
+				insertBack(pst , makeNode('0'));
+				insertBack(answer, makeNode(a_head + b_head));
+				deleteHead(a);
+				deleteHead(b);
 			}
 			else if (a_head && b_head == '.') {
-				insertBack(pList answer, makeNode('.'));
-				deleteHead(pList a);
-				deleteHead(pList b);
+				insertBack(answer, makeNode('.'));
+				deleteHead(a);
+				deleteHead(b);
 			}
 		}
 		insertBack(pst, makeNode('0'));
-		// . 삽입
-
-		if (searchNode(pList pst , '1') == 0) {
+		pst_Pointer = searchNode(answer, '.');
+		insertMid(pst, pst_Pointer, makeNode('.'));
+		// . 삽입 clear
+		if (searchNode(pst , '1') == 0) {
 			return printList(answer);
 		}
-		else if (searchNode(pList pst, '1') != 0) {
-			return add(pList answer, pList pst);
+		else if (isNode(pst, '1') != 0) {
+			return add(answer, pst);
 		}
 	}
 
 	else if ( one == 1 && two == 0) { // add( a,-b )
-		deleteData(pList b, char '-');
+		deleteData(b,'-');
 		return minus(a,b);
 	}
 	else if ( one == 0 && two == 1) { // add(-a,b)
-		deleteData(pList a, char'-');
+		deleteData(a, '-');
 		return minus(b,a);
 	}
 
 	else if ( one == 0 && two == 0) { // add(-a,-b)
-		deleteData(pList a, char'-');
-		deleteData(pList b, char'-');
-		return insertFront(pList add(a,b), makeNode('-'));
-	}
+		deleteData(a, '-');
+		deleteData(b, '-');
+		return insertFront(add(a,b), makeNode('-'));	
 }
 pList minus(pList a , pList b ) {
 
@@ -125,11 +118,11 @@ pList minus(pList a , pList b ) {
 	b_head = (b -> head -> data)
 
 	if ( one == 1 && two == 1) {
-		if (searchNode(pList a, '.') == 0){
-			insertBack(pList a, makeNode('.'));
+		if (searchNode(a, '.') == 0){
+			insertBack(a, makeNode('.'));
 		}
-		if (searchNode(pList b, '.') == 0){
-			insertBack(pList b, makeNode('.'));
+		if (searchNode(b, '.') == 0){
+			insertBack(b, makeNode('.'));
 		}
 		// 자연수일 경우 뒤에 . 추가 
 		if (a_count > b_count) {
@@ -138,29 +131,29 @@ pList minus(pList a , pList b ) {
 		if (a_count < b_count) {
 			count = b_count;
 		}
-		a_point = searchNode(pList a, '.')
-		b_point = searchNode(pList b, '.')
+		a_point = searchNode(a, '.')
+		b_point = searchNode(b, '.')
 		a_int_len = a_point -1;
 		b_int_len = b_point -1;
 		a_dec_len = a_count - a_point;
 		b_dec_len = b_count - b_point;
 		while (a_int_len = b_int_len) {
 			if (a_int_len > b_int_len) {
-				insertFront(pList b, makeNode('0'));
+				insertFront(b, makeNode('0'));
 				b_int_len++;
 			}	
 			else if (a_int_len < b_int_len) {
-				insertFront(pList a, makeNode('0'));
+				insertFront(a, makeNode('0'));
 				a_int_len++;
 			}
 		}
 		while (a_dec_len = b_dec_len) {
 			if (a_dec_len > b_dec_len) {
-				insertBack(pList b, makeNode('0'));
+				insertBack(b, makeNode('0'));
 				b_dec_len++;
 			}
 			else if (a_dec_len < b_dec_len) {
-				insertBack(pList a, makeNode('0'));
+				insertBack(a, makeNode('0'));
 				a_dec_len++;
 			}
 		}
@@ -189,26 +182,27 @@ pList minus(pList a , pList b ) {
 			}
 		}	
 		insertBack(pst, makeNode('0'));
-		// pst 맨뒤 0 추가해 자리수 맞추기
+		pst_Pointer = searchNode(answer, '.');
+		insertMid(pst,pst_Pointer, '.');
 			// answer - 내림수 빼는 재귀 함수 호출
 			// 내림수 에 1 없을때 그냥 값 도출
-		if (searchNode(pst, '1') == 0) {
+		if (isNode(pst, '1') == 0) {
 			return printList(answer);
 		}
-		if (searchNode(pst, '1') != 0) {
+		if (isNode(pst, '1') != 0) {
 			return minus(answer, pst);
 		}
 	}
 	else if ( one == 1 && two == 0) { // minus( a,-b )
-		deleteData( pList b, char '-');
+		deleteData(b, '-');
 		return add (a, b);
 	}		
 	else if ( one == 0 && two == 1) { // minus(-a,b)
-		deleteData(pList a, char'-');
-		return insertFront(pList add(a, b), makeNode('-'));
+		deleteData(a, '-');
+		return insertFront(add(a, b), makeNode('-'));
 	}
 	else if ( one == 0 && two == 0) { // minus( -a , +b )
-		deleteData(pList b, char'-');
+		deleteData(b, '-');
 		return minus(a, b);
 	}
 }
